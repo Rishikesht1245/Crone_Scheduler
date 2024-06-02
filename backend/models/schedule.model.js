9a2b49543b7f8c42138c262470ab13e949863c58
Rishikesh T <rishikeshtgit@gmail.com>
2024-06-02 13:03:58 +0530

    updated the model to add timestamps and changed schedule type to enum

--- backend/models/schedule.model.js
+++ backend/models/schedule.model.js
@@ -1,24 +1,29 @@
 import mongoose, { Schema } from "mongoose";
 
-const ScheduleSchema = new Schema({
-  userId: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
+const ScheduleSchema = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: ["none", "daily", "weekly", "monthly"],
+      default: "none",
+      required: true,
+    },
+    date: {
+      type: Date,
+      default: Date.now,
+    },
   },
-  name: {
-    type: String,
-    required: true,
-  },
-  type: {
-    type: String,
-    required: true,
-  },
-  data: {
-    type: String,
-    default: new Date(),
-  },
-});
+  { timestamps: true }
+);
 
 const Schedule = mongoose.model("Schedules", ScheduleSchema);
 export default Schedule;
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,20 +1,23 @@
 import mongoose, { Schema } from "mongoose";
 
-const UserSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
+const UserSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", UserSchema);
 export default User;
