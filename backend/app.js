2fe75beea5002d652fb75c0a83b2258c619ebe10
Rishikesh T <rishikeshtgit@gmail.com>
2024-06-02 22:12:45 +0530

    Update : modified route for static serving

--- backend/app.js
+++ backend/app.js
@@ -28,13 +28,12 @@ runJobs();
 app.use("/api/v1/users", userRouter);
 
 // for deployment
-// for deployment
 const __dirname = path.resolve();
 console.log(__dirname, "==dirname");
-app.use(express.static(path.join(__dirname, "/dist")));
+app.use(express.static(path.join(__dirname, "/client/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 
 // error handling middleware for asynchronous errors
